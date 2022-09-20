ac3276f3654b23c86f182559767def9094abc425
nadjibabelguembour <nadjibabelguembour@gmail.com>
2022-09-20 21:26:55 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ btn.addEventListener('click', ()=>{
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 })
+
+
+
+document.onclick=function(e){
+    if(e.target.id!== 'menu-btn' && e.target.id!== 'menu'){
+        console.log("you clicked outside the menu")
+        btn.classList.remove('open')
+        // nav.classList.toggle('flex');
+        nav.classList.add('hidden');
+        nav.classList.remove('flex');
+        
+    }
+     
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
   
     screens:{
@@ -17,4 +17,5 @@ module.exports = {
     }
   },
   plugins: [],
+  mode: 'jit'
 }

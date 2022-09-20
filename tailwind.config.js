e393a45434bd11601affc2fb8d5bf0d14374ee3d
nadjibabelguembour <nadjibabelguembour@gmail.com>
2022-09-20 16:54:10 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  
+    screens:{
+      sm:'480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+      
+    },
+    extend: {},
+    container:{
+      center: true,
+      padding: '1rem', 
+    }
+  },
+  plugins: [],
+}
